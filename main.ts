5c12f22e6a63ac6b8ea0fa3ef548ac6c900aac3b
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-01-12 19:59:38 -0600

    Continuing build of Martin Gardner's Hexapawn

--- main.ts
+++ main.ts
@@ -15,6 +15,17 @@ input.onButtonPressed(Button.A, function () {
     }
     ShowBoard()
 })
+input.onButtonPressed(Button.B, function () {
+    if (MOVNUM == 2) {
+        MOVNUM = 4
+        BOARDS = BOARD4
+    } else {
+        if (MOVNUM == 4) {
+            MOVNUM = 6
+            BOARDS = BOARD6
+        }
+    }
+})
 function ShowBoard () {
     basic.showLeds(`
         . . . . .
@@ -35,11 +46,14 @@ function ShowBoard () {
 let NewBoard = ""
 let Y = 0
 let X = 0
+let BOARD6: string[] = []
 let BOARDS: string[] = []
+let BOARD4: string[] = []
 let WHITE: number[] = []
 let BLACK: number[] = []
 let ypos: number[] = []
 let xpos: number[] = []
+let MOVNUM = 0
 basic.showIcon(IconNames.Chessboard)
 basic.pause(100)
 basic.showIcon(IconNames.TShirt)
@@ -51,12 +65,13 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
+MOVNUM = 2
 xpos = [0, 2, 4, 0, 2, 4, 0, 2, 4]
 ypos = [0, 0, 0, 2, 2, 2, 4, 4, 4]
 BLACK = [1, 2, 3]
 WHITE = [7, 8, 9]
-BOARDS = ["123489", "123759", "143559", "123789"]
+let BOARD2 = ["123489", "123759"]
+BOARD4 = ["143559", "235449", "133458", "122469", "235667", "423567", "134668", "126459", "223559", "233557", "133449"]
+BOARDS = BOARD2
+BOARD6 = ["453666", "111456", "224556", "226445", "145666", "356444", "334555", "255444", "225666", "144555", "663555"]
 ShowBoard()
-basic.forever(function () {
-	
-})
